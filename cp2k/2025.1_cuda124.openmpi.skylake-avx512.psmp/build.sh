c77141f7fce4eabcd869948437f6b0438da5b10e
weihong.xu <xuweihong.cn@gmail.com>
2025-04-14 17:23:56 +0800

    fix invalid docker tag

--- cp2k/2025.1_cuda124.openmpi.skylake-avx512.psmp/build.sh
+++ cp2k/2025.1_cuda124.openmpi.skylake-avx512.psmp/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 cd $(dirname $0)
-TAG=cp2k/2025.1+cuda124.openmpi.skylake-avx512.psmp
+TAG=cp2k_2025.1_cuda124.openmpi.skylake-avx512.psmp
 
 docker build --progress plain -t $TAG .
 
