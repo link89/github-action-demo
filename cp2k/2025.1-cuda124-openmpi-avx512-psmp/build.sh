d4392eb6567c13d4e9122aa0b4e594cbe04c7207
weihong.xu <xuweihong.cn@gmail.com>
2025-04-16 21:08:45 +0800

    push image before test

--- cp2k/2025.1-cuda124-openmpi-avx512-psmp/build.sh
+++ cp2k/2025.1-cuda124-openmpi-avx512-psmp/build.sh
@@ -23,17 +23,18 @@ TAG=cp2k:2025.1-cuda124-openmpi-avx512-psmp
 docker build . --build-context dist=/mnt/share/dist --progress plain --tag $TAG \
     --label "runnumber=${GITHUB_RUN_ID}"
 
-# test
-docker run --rm -i $TAG bash <<EOF
-source /opt/cp2k/tools/toolchain/install/setup
-/opt/cp2k/tests/do_regtest.py --mpiexec "mpiexec --bind-to none" --maxtasks 4 --workbasedir /mnt $* /opt/cp2k/exe/local psmp
-EOF
-
-
 # publish
 # https://docs.github.com/en/packages/managing-github-packages-using-github-actions-workflows/publishing-and-installing-a-package-with-github-actions
 
 IMAGE_URL=ghcr.io/link89/$TAG
 echo "$GITHUB_TOKEN" | docker login ghcr.io -u link89 --password-stdin
-docker tag $IMAGE_NAME $IMAGE_URL
+docker tag $TAG $IMAGE_URL
 docker push $IMAGE_URL
+
+# test
+docker run --rm -i $TAG bash <<EOF || true
+env
+source /opt/cp2k/tools/toolchain/install/setup
+env
+/opt/cp2k/tests/do_regtest.py --mpiexec "mpiexec --bind-to none" --maxtasks 4 --workbasedir /mnt $* /opt/cp2k/exe/local psmp
+EOF
