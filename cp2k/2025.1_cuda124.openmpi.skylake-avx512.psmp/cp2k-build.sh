e7590b05ae065e1235ccc8bf634e581adb3e8655
weihong.xu <xuweihong.cn@gmail.com>
2025-04-16 11:54:32 +0800

    full build

--- cp2k/2025.1_cuda124.openmpi.skylake-avx512.psmp/cp2k-build.sh
+++ cp2k/2025.1_cuda124.openmpi.skylake-avx512.psmp/cp2k-build.sh
@@ -18,6 +18,7 @@ popd
 pushd /opt/cp2k/tools/toolchain
 ./install_cp2k_toolchain.sh -j $(nproc) \
     --target-cpu=skylake-avx512 \
+    --install-all \
     --with-cusolvermp=no \
     --enable-cuda=no \
     --with-gcc=system \
